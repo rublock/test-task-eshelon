3343f1be4259d52e1f30e69d1b6dcbc055093760
mack <hypermail@yandex.ru>
2024-08-18 10:41:56 +0300

    commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 os.environ["PATH"] += os.pathsep + "drivers/chrome"
 
@@ -21,14 +23,50 @@ driver.find_element(By.NAME, "Password").send_keys("admin")
 driver.find_element(By.CLASS_NAME, "button-submit").click()
 
 driver.find_element(By.CLASS_NAME, "license-expires-button-cancel").click()
-driver.find_element(By.CLASS_NAME, "header-menu__item").click()
-driver.find_element(By.CLASS_NAME, "idbmXZ").click()
 
-address_type_input = driver.find_element(By.NAME, "AddressType")
-driver.execute_script("arguments[0].value = arguments[1];", address_type_input, "IPv4")
+# Добавление тэга
+driver.find_element(By.XPATH, "//div[text()='Инструменты']").click()
+driver.find_element(By.XPATH, "//a[text()='Теги']").click()
+driver.find_element(By.XPATH, "//span[text()='Добавить тег']").click()
+driver.find_element(By.NAME, "Name").send_keys("Тестовый тэг")
+driver.find_element(By.NAME, "Description").send_keys("Опистание тестового тэга")
+driver.find_element(By.XPATH, "//button[text()='Создать']").click()
+
+driver.find_element(By.XPATH, "//a[text()='Активы']").click()
+driver.find_element(By.XPATH, "//button[text()='Добавить актив']").click()
+
+driver.find_element(By.NAME, "name").send_keys("Тестовый актив")
+driver.find_element(By.NAME, "Address").send_keys("147.45.235.190")
+
+# TODO данный блок вывести в отдельный файл select_dropdown.py
+try:
+    dropdown_wrapper = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//input[@name='AddressType']/parent::div"))
+    )
+    dropdown_wrapper.click()
+except Exception as e:
+    print(f"Ошибка при клике на обертку: {e}")
+
+try:
+    option_to_select = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "IPv4")))
+    option_to_select.click()
+except Exception as e:
+    print(f"Ошибка: {e}")
 
 importance_type_input = driver.find_element(By.NAME, "importanceType")
 driver.execute_script("arguments[0].value = arguments[1];", importance_type_input, "Высокий")
+
+os_type_input = driver.find_element(By.NAME, "osType")
+driver.execute_script("arguments[0].value = arguments[1];", os_type_input, "MacOS")
+
+device_type_input = driver.find_element(By.NAME, "deviceType")
+driver.execute_script("arguments[0].value = arguments[1];", device_type_input, "терминал")
+
+driver.find_element(By.CLASS_NAME, "select-tag-input__icon").click()
+driver.find_element(By.CLASS_NAME, "checkbox__icon").click()
+driver.find_element(By.XPATH, "//button[text()='Создать']").click()
+
+driver.find_element(By.XPATH, "//div[text()='Тестовый актив']").click()
 
 time.sleep(20)
 driver.quit()
